e8ed0dffba466b7dd716968701e564ea237998de
Raj Vamsee <vamsee2312@gmail.com>
2022-02-03 21:28:09 -0500

    HomeComponent Layout

--- src/webapp/myreact/src/index.js
+++ src/webapp/myreact/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
