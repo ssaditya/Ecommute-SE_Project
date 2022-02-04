551bf29de2cd0edf15c5c4b687ef2f8ac76613d4
Sai Aditya Sandupatla <ssaditya2116@gmail.com>
2022-02-04 12:57:22 -0500

    Initial flow for Create User API

---
+++ src/main/models/USER_INFORMATION.go
@@ -0,0 +1,18 @@
+package models
+
+type USER_INFORMATION struct {
+	User_id  int     `gorm:"primary_key;auto_increment;not_null;unique" form:"user_id" json:"user_id"`
+	Username string  `gorm:"not_null" form:"username" json:"username" binding:"required"`
+	Password string  `gorm:"not_null" form:"password" json:"password" binding:"required"`
+	Fname    string  `gorm:"not_null" form:"fname" json:"fname" binding:"required"`
+	Lname    string  `gorm:"not_null" form:"lname" json:"lname" binding:"required"`
+	DOB      string  `form:"dob" json:"dob"`
+	Phoneno  string  `gorm:"not_null" form:"phoneno" json:"phoneno" binding:"required"`
+	Credits  int     `form:"credits" json:"credits"`
+	Carname  string  `form:"carname" json:"carname"`
+	Bio      string  `form:"bio" json:"bio"`
+	R_rating float32 `form:"r_rating" json:"r_rating"`
+	D_rating float32 `form:"d_rating" json:"d_rating"`
+	R_count  int     `form:"r_count" json:"r_count"`
+	D_count  int     `form:"d_count" json:"d_count"`
+}
--- src/main/routers/routers.go
+++ src/main/routers/routers.go
@@ -1,31 +1,26 @@
 package routers
 
-import (
-	"net/http"
+// import (
+// 	"github.com/gin-gonic/gin"
+// )
 
-	"github.com/gin-gonic/gin"
-)
+// func Routers() {
 
-func getPersons(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "getPersons Called"})
-}
-func Routers() {
+// 	r := gin.Default()
+// 	//handlers.getPersonById()
+// 	// API v1
+// 	v1 := r.Group("/api/v1")
+// 	{
+// 		v1.GET("person", handlers.getPersons)
+// 		// v1.GET("person/:id", getPersonById)
+// 		// v1.POST("person", addPerson)
+// 		// v1.PUT("person/:id", updatePerson)
+// 		// v1.DELETE("person/:id", deletePerson)
+// 		// v1.OPTIONS("person", options)
+// 	}
 
-	r := gin.Default()
-	//handlers.getPersonById()
-	// API v1
-	v1 := r.Group("/api/v1")
-	{
-		v1.GET("person", getPersons)
-		// v1.GET("person/:id", getPersonById)
-		// v1.POST("person", addPerson)
-		// v1.PUT("person/:id", updatePerson)
-		// v1.DELETE("person/:id", deletePerson)
-		// v1.OPTIONS("person", options)
-	}
+// 	// By default it serves on :8080 unless a
+// 	// PORT environment variable was defined.
+// 	r.Run()
 
-	// By default it serves on :8080 unless a
-	// PORT environment variable was defined.
-	r.Run()
-
-}
+// }
