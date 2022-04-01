7696204c907251ad7b2f3558156b3cd5ecc6b77e
Sai Swaroop Nimmagadda <swaroop.nimmagadda@outlook.com>
2022-04-01 06:27:37 -0400

    Integrated Rider Component with backend

--- src/webapp/myreact/src/components/ResponsiveDrawer.js
+++ src/webapp/myreact/src/components/ResponsiveDrawer.js
@@ -19,6 +19,8 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@mui/material/Grid';
 import {NavLink} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,14 +37,78 @@ const useStyles = makeStyles((theme) => ({
 const drawerWidth = 240;
 
 function ResponsiveDrawer(props) {
+  const [pickup, setPickup] = useState("")
+  const [dropOff, setDropOff] = useState("")
+  const [time, setTime] = useState("")
+  const [date, setDate] = useState("")
+  const [seats, setSeats] = useState(1)
   const classes = useStyles();
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  const findMonth = (month) => {
+    for(let i = 0; i < 12; i++) {
+      if(month === months[i] && i < 10) {
+        let x = i + 1;
+        return "0" + x;
+      }
+      else if(month === months[i] && i >= 10) return i + 1;
+    }
+  }
+
+  const pickUpChange = e => {
+    setPickup(e.target.value);
+    console.log(pickup)
+  }
+
+  const dropOffChange = e => {
+    setDropOff(e.target.value);
+    console.log(dropOff)
+  }
+
+  const onSelectDate = (datetime) => {
+    let dateTime = datetime.toString();
+    let c = dateTime.substring(17, 24)
+    setTime(c);
+    console.log(dateTime.substring(4,7))
+    let month = findMonth(dateTime.substring(4,7))
+    let day = dateTime.substring(8,10)
+    let year = dateTime.substring(11,15)
+    setDate(month + "/" + day + "/" + year)
+    console.log(date)
+  }
+
+  const onSelectSeats = (seat) => {
+    setSeats(parseInt(seat))
+    console.log(seat)
+  }
+
+  useEffect(() => {
+    
+  }, [])  
+
+  const createTrip = () => {
+    axios.post('http://localhost:8181/createTrip', {
+      "driver_id":4,
+      "source":pickup,
+      "destination":dropOff,
+      "date_of_trip":date,
+      "time_of_trip": time,
+      "no_of_seats": seats
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
+  
   const drawer = (
     <div>
     <div>
@@ -106,26 +172,26 @@ function ResponsiveDrawer(props) {
           open
         >
           {drawer}
-        </Drawer>
+          </Drawer>
       </Box>
       <Box id='riderForm'>
       <Grid container direction="column" justifyContent="space-evenly" alignItems="center"> 
         <Grid item >
         <form className={classes.root} noValidate autoComplete="off">
-              <TextField id="outlined-basic" label="Pickup" variant="outlined" />
+              <TextField id="outlined-basic" label="Pickup" variant="outlined" onChange={pickUpChange}/>
               <ArrowForwardIcon style={{marginTop: 20}}/>
-              <TextField id="outlined-basic" label="Drop Off" variant="outlined" />
+              <TextField id="outlined-basic" label="Drop Off" variant="outlined" onChange={dropOffChange} />
         </form>
         </Grid>
         <Grid item >
-        <div className={classes.items}> <Time /> </div>
+        <div className={classes.items}> <Time onChange={onSelectDate}/> </div>
         </Grid>
         <Grid item>
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className={classes.items}><DropDown /></div>
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}} className={classes.items}><DropDown onChange={onSelectSeats} /></div>
           </Grid>
           <Grid item>
         <div className={classes.items}>
-        <Button variant="outlined" style={{width: 260}}><NavLink className="nav-link" to="/riderconfirmation">
+        <Button variant="outlined" style={{width: 260}} onClick={createTrip} ><NavLink className="nav-link" to="/riderconfirmation">
                   Request Ride
                 </NavLink></Button>
         </div>
