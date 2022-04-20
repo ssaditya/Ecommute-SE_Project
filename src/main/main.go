3dac13b0f79ab0f678451819269552630a7ce51a
Vaishnavi Donepudi <airfairyd@gmail.com>
2022-04-19 20:46:53 -0400

    Modified main.go

--- src/main/main.go
+++ src/main/main.go
@@ -67,7 +67,9 @@ func SetupRouter(db *gorm.DB, storeName string, sessionName string) *gin.Engine 
 	r.GET("/getDriverUpcomingTrips/:driver_id", v.GetDriverUpcomingTrips(db))
 	r.GET("/getRiderUpcomingTrips/:rider_id", v.GetRiderUpcomingTrips(db))
 
-
+	//****PAST TRIP END POINTS
+	r.POST("/createPastTrip", v.CreatePastTrip(db))
+	r.GET("/getPastTripsById/:Mode/:Id", v.GetPastTripsById(db))
 	return r
 }
 
